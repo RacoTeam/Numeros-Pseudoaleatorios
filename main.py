548ade72ba3bf7362a8c163358095f7dcd6aca0e
Lucas Depetris <lucasdepetris14@gmail.com>
2023-05-10 13:45:01 -0300

    cambio en ingreso manual

--- main.py
+++ main.py
@@ -20,15 +20,18 @@ def limpiar_consola():
 
 def ingreso_manual():
     numeros = []
+    i = 1
     while True:
         try:
-            numero = float(input("Ingrese un número (ingrese una tecla no numérica para finalizar): "))
+            print("Ingrese un número")
+            print("Para finalizar ingrese una tecla no númerica")
+            numero = float(input("n" + str(i) + ": "))
+            i += 1
             numeros.append(numero)
             limpiar_consola()
             print("Números ingresados:", numeros)
         except ValueError:
-            break
-    return numeros
+            return numeros
     
 def menu_generadores():
     print("GENERADORES:")
@@ -72,55 +75,37 @@ def menu_generadores():
     return listaGenerada
 
 def menu_pruebas(listaPrueba):
-    if len(listaPrueba) == 0:
-        print("Para realizar las pruebas estadísticas se necesitan de números pseudo-aleatorios previamente generados.")
-        print("Seleccione la forma de obtención de los números pseudo-aleatorios")
-        print("Opción 1: Ingresarlos manualmente")
-        print("Opción 2: Generarlos a través de un generador")
-        try:
-            opcion = int(input("Opción: "))
-            limpiar_consola()
-            if opcion == 1:
-                listaNrosAleatorios = ingreso_manual()
-            elif opcion == 2:
-                listaNrosAleatorios = menu_generadores()
-            else:
-                print("Opción invalida")
-        except:
-            if opcion:
-                print("Error. Seleccione una opción válida.")
-    else:
-        print("PRUEBAS ESTADÍSTICAS:")
-        print("1. Prueba de los promedios")
-        print("2. Prueba de la frecuencia")
-        print("3. Prueba de la serie")
-        print("4. Prueba de Kolmogorov-Smirnov (K-S)")
-        print("5. Prueba de corrida arriba y abajo de la media")
-        print("0. Volver")
-        print("")
-        
-        seleccion = input("Opción: ")
-        try:
-            seleccion = int(seleccion)
-            limpiar_consola()
+    print("PRUEBAS ESTADÍSTICAS:")
+    print("1. Prueba de los promedios")
+    print("2. Prueba de la frecuencia")
+    print("3. Prueba de la serie")
+    print("4. Prueba de Kolmogorov-Smirnov (K-S)")
+    print("5. Prueba de corrida arriba y abajo de la media")
+    print("0. Volver")
+    print("")
+    
+    seleccion = input("Opción: ")
+    try:
+        seleccion = int(seleccion)
+        limpiar_consola()
 
-            if seleccion == 1:
-                Promedios(listaPrueba)
-            elif seleccion == 2:
-                Frecuencia(listaPrueba)
-            elif seleccion == 3:
-                Serie(listaPrueba)
-            elif seleccion == 4:
-                PruebaKS(listaPrueba)
-            elif seleccion == 5:
-                CorridaAyA(listaPrueba)
-            elif seleccion == 0:
-                return
-            else:
-                print("Error. Seleccione una opción válida.")
-        except:
-            if seleccion:
-                print("Error. Seleccione una opción válida.")
+        if seleccion == 1:
+            Promedios(listaPrueba)
+        elif seleccion == 2:
+            Frecuencia(listaPrueba)
+        elif seleccion == 3:
+            Serie(listaPrueba)
+        elif seleccion == 4:
+            PruebaKS(listaPrueba)
+        elif seleccion == 5:
+            CorridaAyA(listaPrueba)
+        elif seleccion == 0:
+            return
+        else:
+            print("Error. Seleccione una opción válida.")
+    except:
+        if seleccion:
+            print("Error. Seleccione una opción válida.")
 
 #Funciona como un main. Indica que empieza a ejecutar aqui
 if __name__ == '__main__':
@@ -145,6 +130,7 @@ if __name__ == '__main__':
         print("--------------------------------------------------")
         print("Seleccione una opción")
         print("1. Generadores\t\t2. Pruebas")
+        print("3. Ingresar datos manualmente")
         print("0. Salir")
         print("")
 
@@ -157,7 +143,27 @@ if __name__ == '__main__':
             if seleccion == 1:
                 listaNrosAleatorios = menu_generadores()
             elif seleccion == 2:
-                menu_pruebas(listaNrosAleatorios)
+                if len(listaNrosAleatorios) == 0:
+                    print("Para realizar las pruebas estadísticas se necesitan de números pseudo-aleatorios previamente generados.")
+                    print("Seleccione la forma de obtención de los números pseudo-aleatorios")
+                    print("Opción 1: Ingresarlos manualmente")
+                    print("Opción 2: Generarlos a través de un generador")
+                    try:
+                        opcion = int(input("Opción: "))
+                        limpiar_consola()
+                        if opcion == 1:
+                            listaNrosAleatorios = ingreso_manual()
+                        elif opcion == 2:
+                            listaNrosAleatorios = menu_generadores()
+                        else:
+                            print("Opción invalida")
+                    except:
+                        if opcion:
+                            print("Error. Seleccione una opción válida.")
+                else:
+                    menu_pruebas(listaNrosAleatorios)
+            elif seleccion == 3:
+                listaNrosAleatorios = ingreso_manual()
             elif seleccion == 0:
                 bandera = False
             else:
