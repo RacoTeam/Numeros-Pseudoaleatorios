68c0937ef8427b6426a9c426b847925be2f226b5
Lucas Depetris <lucasdepetris14@gmail.com>
2023-05-12 03:19:59 -0300

    Agrego CorridaAyA

--- pruebas/CorridaAyA.py
+++ pruebas/CorridaAyA.py
@@ -1,46 +1,78 @@
 import sys
 import msvcrt
+from itertools import groupby
+
+# Son las 3am esto es un quilombo no preguntes como funciona
+def obtener_frec_obs(listaConsec):
+
+    listaConsec = [list(g) for k, g in groupby(listaConsec, lambda x:x>0)]
+
+    # print(str(listaConsec))
+
+    listaConsec.sort(key=len)
+    listaConsec.reverse()
+
+    # print(str(listaConsec))
+
+    listaFrecObs = []
+
+    max_long = len(listaConsec[0])
+
+    for i in range(max_long):
+        longitud = 0
+        for sublist in listaConsec:
+            if len(sublist) == (i + 1):
+                longitud += 1
+        listaFrecObs.append(longitud)
+
+    return listaFrecObs
 
 def CorridaAyA(listaNros):
 
     print("---------------------------------------------")
-    print("----- PRUEBA DE CORRIDA ARRIBA Y ABAJO ------")
+    print("------------ PRUEBA DE CORRIDA --------------")
+    print("-------- ARRIBA Y ABAJO DE LA MEDIA ---------")
     print("---------------------------------------------")
 
-    # try:  
-    d_a = float(input("Ingrese el estadistico d: "))
+    # try:
+
+    # 1. Generar una muestra de n números pseudo-aleatorios ui
+    est_x = float(input("Ingrese el estadístico Xa: "))
     n = len(listaNros)
 
-    listaOrdenada = sorted(listaNros)
-    listaFn = []
+    # 2. A continuación la secuencia binaria S
+    # 3. Determinar las longitudes de corrida (Frecuencia observada de longitud i)
 
-    i = 0
-    while(i<n):
-        listaFn.append(i/n)
-        i=i+1
-    
-    j=0
-    listaResta = []
+    listaConsecutivos = []
+    for num in listaNros:
+        if num <= 0.5:
+            listaConsecutivos.append(0)
+        else:
+            listaConsecutivos.append(1)
 
-    while(j<n):
-        listaResta.append(listaNros[j] - listaOrdenada[j])
-        j = j + 10
-    
-    maximo  = max(listaResta)
+    listaFrecObs = obtener_frec_obs(listaConsecutivos)
 
-    if(maximo < d_a):
-        print("No se rechaza la hipotesis de que los numeros provienen de un universo uniformemente distribuido")
+    # 4. Calcular la Frecuencia esperada para la i-ésima longitud 〖𝐹𝑒〗_𝑖=(𝑛−𝑖+3)/2^(𝑖+1) 
+
+    listaFrecEsp = []
+
+    for i in range(len(listaFrecObs)):
+        listaFrecEsp.append((n - (i + 1) + 3)/pow(2, (i + 1) + 1))
+
+    # 5. Calcular el Estadístico  𝝌2=∑_(𝒊=𝟏)^𝒙▒(〖𝑭𝒐〗_𝒊−〖𝑭𝒆〗_𝒊 )^𝟐/〖𝑭𝒆〗_𝒊 
+
+    # Sumatoria
+    chicuad = 0
+    for i in range(len(listaFrecObs)):
+        print(pow(listaFrecObs[i] - listaFrecEsp[i], 2)/listaFrecEsp[i])
+        chicuad += pow(listaFrecObs[i] - listaFrecEsp[i], 2)/listaFrecEsp[i]
+
+    print("¿" + str(chicuad) + " < " + str(est_x) + "?")
+
+    if(chicuad < est_x):
+        print("No se rechaza la hipótesis de que los números provienen de un universo uniformemente distribuido")
     else:
-        print("Se rechaza la hipotesis de que los numeros provienen de un universo uniformemente distribuido")
-
-    #3 Determinar el valor del estadístico Zo, utilizando la siguiente fórmula 
-    
-    #4. Si |𝒁𝟎|< Zα
-    
-    # except(ValueError):
-    #     print("Tienes un error de tipo: ",sys.exc_info()[0])
-    #     print("Nota: Se debe ingresar un valor de tipo numerico")
+        print("Se rechaza la hipótesis de que los números provienen de un universo uniformemente distribuido")
 
     print("Presione cualquier tecla para continuar...")
     msvcrt.getch()
-
